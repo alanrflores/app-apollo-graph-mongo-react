1133d33a9616cbc6a3c54bb11d348a696bf5b668
alanrflores <alan.flores640@gmail.com>
2023-02-08 00:47:00 -0300

    changes in resolvers and typedefs

--- api/resolvers/resolvers.js
+++ api/resolvers/resolvers.js
@@ -1,162 +1,200 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const Product = require("../models/Product");
 const User = require("../models/User");
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const mercadopago = require('mercadopago');
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const mercadopago = require("mercadopago");
 
 //configure the SDK
-mercadopago.configure({ access_token: process.env.MP_ACCESS_KEY});
-
+mercadopago.configure({ access_token: process.env.MP_ACCESS_KEY });
 
 const resolvers = {
-    Query: {
-        hello: () => 'Hello World',
-        getAllProduct : async() => {
-            const product = await Product.find()
-            return product;
-        },
-        //no tengo una consulta para el parent por esos el guion bajo
-        findProduct: async(_, args) => {
-            const productId = await Product.findById(args.id)
-            return productId;
-        },
-       getAllUser: async(_,) => {
-            const userAll = await User.find()
-            return userAll;
-       },
-       user: async(_, args) => {
-          const userId = await User.findById(args.id)
-          return userId;
-        },
-        userData: async(parent, args, context) => {
-            console.log('context', context)
-            if(!context.user) {
-              throw new Error('Not authenticated');
-            }
-            const data = getUserData();
-            return data;
-        },
-        adminData: async(root, args, context) => {
-           if(!context.user || !context.user.role === 'ADMIN') {
-                throw new Error('Error Unauthorized');
-           }
-           const data = getAdminData();
-           return data;
-        },
+  Query: {
+    hello: () => "Hello World",
+    getAllProduct: async () => {
+      const product = await Product.find();
+      return product;
     },
-    Mutation: {
-        
-        createProduct: async(_, args) => {
-            const { title, price, description, quantity, stock, images } = args.product;
-             try {
-                const newProduct = new Product({title, price, description, quantity, stock, images})
-                console.log(newProduct); 
-                const savedProduct = await newProduct.save();
-                return savedProduct;
-            } catch (error) {
-                throw new Error(error);
-            }
+    //no tengo una consulta para el parent por esos el guion bajo
+    findProduct: async (_, args) => {
+      const productId = await Product.findById(args.id);
+      return productId;
+    },
+    getAllUser: async (_) => {
+      const userAll = await User.find();
+      return userAll;
+    },
+    user: async (_, args) => {
+      const userId = await User.findById(args.id);
+      return userId;
+    },
+    userData: async (parent, args, context) => {
+      console.log("context", context);
+      if (!context.user) {
+        throw new Error("Not authenticated");
+      }
+      const data = getUserData();
+      return data;
+    },
+    adminData: async (root, args, context) => {
+      if (!context.user || !context.user.role === "ADMIN") {
+        throw new Error("Error Unauthorized");
+      }
+      const data = getAdminData();
+      return data;
+    },
+  },
+  Mutation: {
+    createProduct: async (_, args) => {
+      const { title, price, description, quantity, stock, images } =
+        args.product;
+
+      try {
+        const newProduct = new Product({
+          title,
+          price,
+          description,
+          quantity,
+          stock,
+          images,
+        });
+        console.log(newProduct);
+        const savedProduct = await newProduct.save();
+        return savedProduct;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    deleteProduct: async (_, args) => {
+      await Product.findByIdAndDelete(args.id);
+      return "Product DELETE";
+    },
+    updateProduct: async (_, { product, id }) => {
+      // console.log(product)
+      const updateProduct = await Product.findByIdAndUpdate(
+        { _id: id },
+        {
+          //$set la propiedad set actualiza todos los campos , es mas generico.
+          $set: {
+            ...product,
+          },
         },
-        deleteProduct: async(_, args) => {
-            await Product.findByIdAndDelete(args.id)
-            return "Product DELETE"
+        { new: true }
+      );
+      return updateProduct;
+    },
+
+    updateProductStock: async (_, { stock, id }) => {
+      const updateProductStock = await Product.findByIdAndUpdate(
+        { _id: id },
+        {
+          stock: stock,
         },
-        updateProduct: async(_, {product, id}) => {
-            //console.log(product)
-            const ProductUpdate = await Product.findByIdAndUpdate(id,{
-                //propiedad $set actualiza los campos que trae Product, es mas generico 
-                $set: product
-            }, {new: true} );
-            return ProductUpdate;
-        },
-        updateRole: async(_, {role, id}) => {
-          const roleUpdate = await User.findByIdAndUpdate(id, {role},{new: true});
-          return roleUpdate;
-        },
+        { new: true }
+      );
+      return updateProductStock;
+    },
 
-        createPayment: async(_, args , context) => {
-          console.log('context', context)
-          try {
-            const product = await Product.findById(args.productId);  
-            const payment = await mercadopago.preferences.create({
-                back_urls: {
-                    success: 'http://localhost:5173/success',
-                    failure: '',
-                    pending: '',
-                 },
-                items: [
-                    {   
-                        transaction_amount: product?.price * args.quantity,
-                        title: product?.title,
-                        description: product?.description,
-                        quantity: args.quantity,
-                        currency_id: 'USD',
-                        unit_price: product?.price
-                    }
-                ],
-                payer: {
-                    email: context?.user?.email,
-                 },
-            });
-            console.log('payment', payment.body)
-            return payment.body;
-          } catch (error) {
-            throw new Error(error)
-          } 
-        },
+    updateRole: async (_, { role, id }) => {
+      const roleUpdate = await User.findByIdAndUpdate(
+        id,
+        { role },
+        { new: true }
+      );
+      return roleUpdate;
+    },
 
-        registerUser: async(_, {registerInput: {username, email, password, role } }) => {
-            const oldUser = await User.findOne({ email })
-            if(oldUser){
-                throw new Error('A user is already registered with the email' + email)
-            }
-            const hashed = await bcrypt.hash(password, 10)
+    createPayment: async (_, { productIds, quantities }, context) => {
+      try {
+        const products = await Product.find({ _id: { $in: productIds } });
 
-            const newUser = new User({
-                  username: username,
-                  email: email,
-                  password: hashed,
-                  role: role
-              });
+        var items = products?.map((product, index) => {
+          return {
+            title: product?.title,
+            description: product?.description,
+            quantity: quantities[index],
+            currency_id: "USD",
+            unit_price: product?.price,
+          };
+        });
 
-              const token = jwt.sign({ id: newUser._id, email} , process.env.JWT_SECRET, {
-                  expiresIn: "2h"
-              });
+        const payment = await mercadopago.preferences.create({
+          items: items,
+          payer: {
+            email: context?.user?.email,
+          },
+          back_urls: {
+            success: "http://localhost:5173/success",
+            failure: "",
+            pending: "",
+          },
+        });
+        console.log("payment", payment.body);
+        return payment.body;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
 
-              newUser.token = token ;
-              const response = await newUser.save();
+    registerUser: async (
+      _,
+      { registerInput: { username, email, password, avatar }, role: role }
+    ) => {
+      const oldUser = await User.findOne({ email });
+      if (oldUser) {
+        throw new Error("A user is already registered with the email" + email);
+      }
+      const hashed = await bcrypt.hash(password, 10);
 
-              return {
-                  id: response.id,
-                  ...response._doc
-              }
-        },
-        loginUser: async(_, {loginInput: {email, password} }) => {
-            
-             const user = await User.findOne({ email });
-             console.log("user", user)
-             const valid = await bcrypt.compare(password, user.password);
-             if(user && valid){
-                const token = jwt.sign({ id: user._id, email} , process.env.JWT_SECRET, {
-                    expiresIn: "2h"
-                });
-                user.token = token;
+      const newUser = new User({
+        username: username,
+        email: email,
+        password: hashed,
+        avatar: avatar,
+        role: role,
+      });
 
-                return {
-                    id: user.id,
-                    ...user._doc
-                }
-            }else{
-               throw new Error("Incorrect Password!")
-            }
-            //   return jwt.sign({ id: user._id}, process.env.JWT_SECRET); 
-        },
+      const token = jwt.sign(
+        { id: newUser._id, email },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "2h",
+        }
+      );
 
-    }
+      newUser.token = token;
+      const response = await newUser.save();
 
+      return {
+        id: response.id,
+        ...response._doc,
+      };
+    },
+    loginUser: async (_, { loginInput: { email, password } }) => {
+      const user = await User.findOne({ email });
+      console.log("user", user);
+      const valid = await bcrypt.compare(password, user.password);
+      if (user && valid) {
+        const token = jwt.sign(
+          { id: user._id, email },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "2h",
+          }
+        );
+        user.token = token;
+
+        return {
+          id: user.id,
+          ...user._doc,
+        };
+      } else {
+        throw new Error("Incorrect Password!");
+      }
+      //   return jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+    },
+  },
 };
 
-module.exports = { resolvers } ;
+module.exports = { resolvers };
--- api/typeDefs/typeDefs.js
+++ api/typeDefs/typeDefs.js
@@ -1,109 +1,125 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  directive @auth(requires: Role) on OBJECT | FIELD_DEFINITION
 
-directive @auth(requires: Role) on OBJECT | FIELD_DEFINITION
+  enum Role {
+    USER
+    ADMIN
+  }
 
-enum Role {
-  USER
-  ADMIN
-}
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    avatar: String
+    role: Role!
+    token: String
+  }
 
-type User {
-  id: ID!
-  username:String!
-  email: String!
-  password: String!
-  avatar: String
-  role: Role!
-  token: String
-}
+  input RegisterInput {
+    username: String!
+    email: String!
+    password: String!
+    avatar: String
+  }
 
-input RegisterInput {
-  username: String!
-  email: String!
-  password: String!
+  input LoginInput {
+    email: String!
+    password: String!
+  }
 
-}
+  type Image {
+    id: ID!
+    url: String!
+    title: String!
+  }
 
-input LoginInput {
-  email: String!
-  password: String!
-}
-
-type Image {
-  id: ID!
-  url: String!
-  title: String!
-}
-
- type Product {
-   id: ID
-   title: String!
-   price: String!
-   description: String!
-   quantity: Int!
-   stock: String!
-   images: [Image]
-   paymentId: ID
- }
-
-
- type Payment {
+  type Product {
     id: ID
+    title: String!
+    price: String!
+    description: String!
+    quantity: Int!
+    stock: String!
+    images: [Image]
+    paymentId: ID
+  }
+
+  type Payer {
+    email: String
+  }
+
+  type Item {
+    title: String
+    description: String
+    quantity: Int
+    unit_price: String
+  }
+
+  type Payment {
+    id: ID
+    productIds: [ID]
+    quantities: [Int]
     init_point: String
-    operation_type: String     
-}
+    operation_type: String
+    items: [Item]
+    payer: Payer
+  }
 
-input ImageInput {
-  url: String!
-  title: String!
-}
- 
-# input CardInput {
-#     card_number: String!
-#     expiration_month: String!
-#     expiration_year: String!
-#     security_code: String!
-#     cardholder_name: String!
-# }
+  input ImageInput {
+    url: String!
+    title: String!
+  }
 
-input ProductInput {
-   title: String!
-   price: String!
-   description: String!
-   quantity: Int!
-   stock: String!
-   images: [ImageInput]
- }
+  input PaymentInput {
+    productId: ID!
+    quantity: Int!
+  }
+  # input CardInput {
+  #     card_number: String!
+  #     expiration_month: String!
+  #     expiration_year: String!
+  #     security_code: String!
+  #     cardholder_name: String!
+  # }
 
- type Data {
-  id: ID!
-  value: String
-}
- 
- type Query {
-   hello: String,
-   getAllProduct: [Product],
-   findProduct(id: ID): Product,
-   user(id: ID!) : User,
-   getAllUser: [User],
-   userData: [Data] @auth(requires: USER)
-   adminData: [Data] @auth(requires: ADMIN)
-   data(id: ID!): Data
- }
+  input ProductInput {
+    title: String!
+    price: String!
+    description: String!
+    quantity: Int!
+    stock: String!
+    images: [ImageInput]
+  }
 
- type Mutation {
-   createProduct(product: ProductInput ): Product
-   deleteProduct(id: ID!): String
-   updateProduct(id: ID!, product: ProductInput ) : Product
-   updateRole(id: ID!, role: Role = USER) : User 
-   createPayment(productId: ID!, quantity: Int! ) : Payment
-   registerUser(registerInput: RegisterInput, role: Role) : User
-   loginUser(loginInput: LoginInput) : User
+  type Data {
+    id: ID!
+    value: String
+  }
 
- }
+  type Query {
+    hello: String
+    getAllProduct: [Product]
+    findProduct(id: ID): Product
+    user(id: ID!): User
+    getAllUser: [User]
+    userData: [Data] @auth(requires: USER)
+    adminData: [Data] @auth(requires: ADMIN)
+    data(id: ID!): Data
+  }
 
-`
+  type Mutation {
+    createProduct(product: ProductInput): Product
+    deleteProduct(id: ID!): String
+    updateProduct(id: ID!, product: ProductInput): Product
+    updateProductStock(id: ID!, stock: String): Product
+    updateRole(id: ID!, role: Role = USER): User
+    createPayment(productIds: [ID], quantities: [Int]): Payment
+    registerUser(registerInput: RegisterInput, role: Role): User
+    loginUser(loginInput: LoginInput): User
+  }
+`;
 
 module.exports = { typeDefs };
