5db084392a9ebc04354ae1e996b096251686d117
alanrflores <alan.flores640@gmail.com>
2023-02-15 16:45:27 -0300

    modify commits

--- api/db.js
+++ api/db.js
@@ -1,17 +1,16 @@
 //esto apenas arranque la aplicación va a leer el archivo .env
-require("dotenv").config();
-
-const { connect } = require("mongoose");
+require('dotenv').config();
+const { connect } = require('mongoose');
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 const connectDb = async () => {
-  try {
-    await connect(process.env.MONGO_DB);
-    console.log("Mongo db connect");
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+      await connect(process.env.MONGO_DB)  
+      console.log("Mongo db connect");
+    } catch (error) {
+      console.error(error)  
+    }
 };
 
-module.exports = { connectDb };
+module.exports =  { connectDb };
